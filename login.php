23dff52291647e873ed8d66623a55572a52932b9
nikhilnc1 <98471734+nikhilnc1@users.noreply.github.com>
2022-04-21 15:12:58 +0530

    add boxes

--- login.php
+++ login.php
@@ -10,227 +10,175 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+P+One&display=swap" rel="stylesheet">
     <style>
-        * {
-            box-sizing: border-box;
-            text-align: center;
+    * {
+        box-sizing: border-box;
+        text-align: center;
 
-        }
-
-
-        a {
-            text-decoration: none;
-
-        }
-
-        a:hover {
-            color: black;
-        }
+    }
 
 
-        body {
-            font-family: Mochiy Pop P One;
-            margin: 5% 10% 5% 5%;
-            font-size: 17px;
-            padding: 8px;
-            background-color: rgb(173, 212, 230);
+    a {
+        text-decoration: none;
+
+    }
+
+    a:hover {
+        color: black;
+    }
 
 
-        }
-
-        .parent {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-
-
-        .container1{
-            border-right: 2px solid black;
-            padding-right: 100px;
-        }
-
-        .container {
-            text-align: center;
-            background-color: #f2f2f2;
-            width: 400px;
-            height: 500px;
-            padding: 50px 20px 0px 20px;
-            border: 1px solid lightgray;
-            border-radius: 30px;
-            
+    body {
+        font-family: Mochiy Pop P One;
+        margin: 5% 10% 5% 5%;
+        font-size: 17px;
+        padding: 8px;
+        background-color: rgb(173, 212, 230);
 
 
+    }
 
-        }
+    .parent {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
 
-        .container2 {
-            padding-left: 2 0px;
-            /* padding */
-        }
+
+    .container1 {
+        border-right: 2px solid black;
+        padding-right: 100px;
+    }
+
+    .container {
+        text-align: center;
+        background-color: #f2f2f2;
+        width: 400px;
+        height: 500px;
+        padding: 50px 20px 0px 20px;
+        border: 1px solid lightgray;
+        border-radius: 30px;
 
 
+
+
+    }
+
+    .container2 {
+        padding-left: 2 0px;
+        /* padding */
+    }
 
 
 
 
 
-        input[type="password"],
-        input[type="email"] {
-            font-family: Mochiy Pop P One;
-            width: 100%;
-            padding: 15px;
-            border: 10px;
-            border: 1px solid grey;
-            border-radius: 30px;
-            color: rgb(88, 45, 138);
-            font-weight: bolder;
-
-            /* background: linear-gradient(145deg, #2149cc, #b44747); */
-            box-shadow: 8px 8px 16px #aaaaaa,
-                -8px -8px 16px #ffffff;
 
 
-        }
+    input[type="password"],
+    input[type="email"] {
+        font-family: Mochiy Pop P One;
+        width: 100%;
+        padding: 15px;
+        border: 10px;
+        border: 1px solid grey;
+        border-radius: 30px;
+        color: rgb(88, 45, 138);
+        font-weight: bolder;
+
+        /* background: linear-gradient(145deg, #2149cc, #b44747); */
+        box-shadow: 8px 8px 16px #aaaaaa,
+            -8px -8px 16px #ffffff;
 
 
-
-
-
-        input[type="email"] {
-            background-color: rgb(207, 210, 231);
-            margin-top: 10px;
-            margin-bottom: 10px;
-
-        }
-
-        input[type="password"] {
-            background-color: rgb(190, 194, 224);
-            margin-bottom: 10px;
-        }
-
-
-        ::placeholder {
-            color: rgb(57, 17, 97);
-            font-weight: bolder;
-            font-family: Mochiy Pop P One;
-        }
+    }
 
 
 
 
-        input[type="email"]:hover,
-        input[type="password"]:hover {
-            background-color: rgba(78, 79, 80, 0.322);
-        }
 
-        /* @media screen and (min-width:1000px) {
-            button {
-                margin-left: 40%;
-            }
+    input[type="email"] {
+        background-color: rgb(207, 210, 231);
+        margin-top: 10px;
+        margin-bottom: 10px;
 
-        }
+    }
 
-        @media screen and (min-width:701px) and (max-width:1000px) {
-            button {
-                margin-left: 35%;
-            }
-        }
-
-        @media screen and (min-width:401px) and (max-width:700px) {
-            button {
-                margin-left: 25%;
-            }
-        }
-
-        @media screen and (max-width:400px) {
-
-            .container {
-                text-align: center;
-                background-color: #f2f2f2;
-                padding: 10px 20px 30px 20px;
-                border: 1px solid lightgray;
-                border-radius: 30px;
-                width: 100%;
-                height: 300px; 
-                margin-top: 10px;
-                margin-bottom: 10px;
+    input[type="password"] {
+        background-color: rgb(190, 194, 224);
+        margin-bottom: 10px;
+    }
 
 
-            }
+    ::placeholder {
+        color: rgb(57, 17, 97);
+        font-weight: bolder;
+        font-family: Mochiy Pop P One;
+    }
 
 
 
-            body {
-                font-family: Mochiy Pop P One;
-                margin: 3% 3%;
-                margin-top: 50px;
-                margin-bottom: 50px;
-                font-size: 17px;
-                padding: 0px;
 
+    input[type="email"]:hover,
+    input[type="password"]:hover {
+        background-color: rgba(78, 79, 80, 0.322);
+    }
 
+    .cta {
+        position: relative;
+        margin: auto;
+        padding: 12px 18px;
+        transition: all 0.2s ease;
+        border: none;
+        background: none;
+    }
 
-            }
+    .cta:before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        display: block;
+        border-radius: 50px;
+        background: #b1dae7;
+        width: 45px;
+        height: 45px;
+        transition: all 0.3s ease;
+    }
 
-        } */
+    .cta span {
+        position: relative;
+        font-family: "Ubuntu", sans-serif;
+        font-size: 18px;
+        font-weight: 700;
+        letter-spacing: 0.05em;
+        color: #234567;
+    }
 
-        /* From cssbuttons.io by @alexmaracinaru */
-        .cta {
-            position: relative;
-            margin: auto;
-            padding: 12px 18px;
-            transition: all 0.2s ease;
-            border: none;
-            background: none;
-        }
+    .cta svg {
+        position: relative;
+        top: 0;
+        margin-left: 10px;
+        fill: none;
+        stroke-linecap: round;
+        stroke-linejoin: round;
+        stroke: #234567;
+        stroke-width: 2;
+        transform: translateX(-5px);
+        transition: all 0.3s ease;
+    }
 
-        .cta:before {
-            content: "";
-            position: absolute;
-            top: 0;
-            left: 0;
-            display: block;
-            border-radius: 50px;
-            background: #b1dae7;
-            width: 45px;
-            height: 45px;
-            transition: all 0.3s ease;
-        }
+    .cta:hover:before {
+        width: 100%;
+        background: #b1dae7;
+    }
 
-        .cta span {
-            position: relative;
-            font-family: "Ubuntu", sans-serif;
-            font-size: 18px;
-            font-weight: 700;
-            letter-spacing: 0.05em;
-            color: #234567;
-        }
+    .cta:hover svg {
+        transform: translateX(0);
+    }
 
-        .cta svg {
-            position: relative;
-            top: 0;
-            margin-left: 10px;
-            fill: none;
-            stroke-linecap: round;
-            stroke-linejoin: round;
-            stroke: #234567;
-            stroke-width: 2;
-            transform: translateX(-5px);
-            transition: all 0.3s ease;
-        }
-
-        .cta:hover:before {
-            width: 100%;
-            background: #b1dae7;
-        }
-
-        .cta:hover svg {
-            transform: translateX(0);
-        }
-
-        .cta:active {
-            transform: scale(0.95);
-        }
+    .cta:active {
+        transform: scale(0.95);
+    }
     </style>
 </head>
 
